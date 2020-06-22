b43a69dc914bf95e1f5402b11f49b9b7d88166e5
Morteza Kheyrifam <morteza.kheyrifam@c02xn0q7jgh7.check24.intern>
2020-06-22 14:37:16 +0200

    add TinyConstraints

--- MortezaPod.podspec
+++ MortezaPod.podspec
@@ -43,4 +43,6 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  
+  s.dependency 'TinyConstraints'
 end
--- Source/SampleClass.swift
+++ Source/SampleClass.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TinyConstraints
 
 public class SampleClass {
     
@@ -15,5 +16,18 @@ public class SampleClass {
 
     public func getSampleText() -> String {
         return "New hello world!"
+    }
+    
+    public func getSampleViewController() -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = "Hello Morteza"
+        label.textColor = .yellow
+        viewController.view.addSubview(label)
+        label.centerInSuperview()
+        
+        return viewController
     }
 }
